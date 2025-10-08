1184d66bb2a19e228c5e956d01c5f4eb46ca4d90
Lucas-Calo <lucaspc.calo@gmail.com>
2025-10-08 11:09:06 -0300

    - Aluno agora pode entregar atividades, alterando o status para 'Aguardando Avaliação'.
    - Professor pode avaliar as entregas com nota e feedback através de um prompt.
    
    - Adiciona indicadores visuais de status (Pendente, Aprovado, etc.) nos painéis.

--- src/pages/ProfessorDashboard.jsx
+++ src/pages/ProfessorDashboard.jsx
@@ -4,7 +4,7 @@ import { useAtividades } from '../contexts/AtividadeContext';
 import './ProfessorDashboard.css';
 
 const ProfessorDashboard = () => {
-  const { atividades, deleteAtividade } = useAtividades();
+  const { atividades, deleteAtividade, updateStatusAtividade } = useAtividades();
 
   const handleDelete = (id) => {
     if (window.confirm('Tem certeza que deseja deletar esta atividade?')) {
@@ -16,6 +16,30 @@ const ProfessorDashboard = () => {
   const formatarData = (dataString) => {
     const [ano, mes, dia] = dataString.split('-');
     return `${dia}/${mes}/${ano}`;
+  };
+
+  //Sprint 4: 
+  const handleAvaliar = (id) => {
+    const nota = prompt("Digite a nota da atividade (0 a 10):");
+    if (nota === null || nota.trim() === '' || isNaN(nota) || nota < 0 || nota > 10) {
+      alert("Nota inválida. A avaliação foi cancelada.");
+      return;
+    }
+
+    const feedback = prompt("Digite um feedback para o aluno:");
+    if (feedback === null) {
+      alert("Avaliação cancelada.");
+      return;
+    }
+
+    const notaNum = parseFloat(nota);
+    const statusFinal = notaNum >= 6 ? 'Aprovado' : 'Reprovado';
+
+    updateStatusAtividade(id, {
+      status: statusFinal,
+      nota: notaNum,
+      feedback: feedback.trim() === '' ? 'Sem feedback.' : feedback
+    });
   };
 
   return (
@@ -38,7 +62,12 @@ const ProfessorDashboard = () => {
               <p>{atividade.descricao}</p>
               <span>Data de Entrega: {formatarData(atividade.dataEntrega)}</span>
               
-              <div className="card-actions">
+             <div className="card-actions">
+                {atividade.status === 'Aguardando Avaliação' && (
+                  <button onClick={() => handleAvaliar(atividade.id)} className="btn-action btn-avaliar">
+                    Avaliar
+                  </button>
+                )}
                 <Link 
                   to={`/professor/editar-atividade/${atividade.id}`} 
                   state={{ atividade: atividade }}
